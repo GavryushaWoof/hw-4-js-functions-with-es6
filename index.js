2c3346694b9e10c720a5e6d8a5216ce3ca3ce7b3
GavryushaWoof <gavgav.cat@yandex.ru>
2020-10-27 07:38:01 +0300

    1-9

--- index.js
+++ index.js
@@ -49,31 +49,19 @@ console.log("---------");
 
 //5) Write a function stringExpansion
 function stringExpansion(str) {
-    // let word = '';
-    // const regExp = new RegExp(/(?<quantity>[0-9])*?(?<char>[a-zA-Z])/g);
-    // let match = null;
-    // while (match = regExp.exec(str)) {
-    //     word += match.groups.char;
-    //     for (let i = match.groups.quantity - 1; i > 0; i--) {
-    //         word += match.groups.char;
-    //     }
-    // }
-    // return word
     let word = '';
     const regExp = new RegExp(/(?<quantity>[0-9])*?(?<char>[a-zA-Z])/g);
     for (const iterator of str.matchAll(regExp)) {
-        word += iterator.groups.char;
-        for (let i = iterator.groups.quantity - 1; i > 0; i--) {
-            word += iterator.groups.char;
-        }
+        let { quantity, char } = iterator.groups;
+        (quantity == undefined) ? word += char : word += char.repeat(quantity);
     }
     return word;
 }
 console.log(stringExpansion('3D2a5d2f'));
 console.log(stringExpansion('3d332f2a'));
 console.log(stringExpansion('abcde'));
-console.log(stringExpansion('232132'));
 console.log(stringExpansion('a232132'));
+console.log(stringExpansion('232132'));
 console.log(stringExpansion(''));
 
 console.log("---------");
@@ -92,12 +80,7 @@ console.log("---------");
 
 //7) Write function `transform` that will transform array of numbers to array of functions that will return value from a base array.
 function transform(array) {
-    // let arrayNew = [];
-    // for (const [key, _] of array.entries()) {
-    //     arrayNew[key] = () => array[key];
-    // }
-    // return arrayNew;
-    return array.map((_, key) => () => array[key]);
+    return array.map((_, index) => () => array[index]);
 }
 let baseArray = [10, 20, 30, 40, 50];
 let newArray = transform(baseArray);
@@ -108,7 +91,7 @@ console.log("---------");
 
 //8) Write function `sum`. Function expects arbitrary number of digit arguments and returns compound value of them.
 function sum(...rest) {
-    return (rest.length === 0) ? 0 : rest[0] + sum.apply(null, rest.slice(1));
+    return (rest.length === 0) ? 0 : rest[0] + sum(...rest.slice(1));
 }
 console.log(sum(1, 3, 5, 7));
 
@@ -116,10 +99,10 @@ console.log("---------");
 
 //9) Write function `countDown`. Function expects number and logs values one by one till zero with one second delay.
 function countDown(num) {
-    for (let count = 1000, i = num; i >= 0; i--, count += 1000) {
+    for (let ms = 1000, i = num; i >= 0; i--, ms += 1000) {
         setTimeout(() =>
             console.log(i),
-            (count));
+            (ms));
     }
 }
 countDown(3);
